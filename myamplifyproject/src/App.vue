78bb550ca50d90a0bcbf8af28f186089b6a67983
Kisky3 <kisky3@126.com>
2020-07-22 00:47:19 +0900

    :sparkles:add information to toso list!

--- myamplifyproject/src/App.vue
+++ myamplifyproject/src/App.vue
@@ -1,28 +1,63 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Todo App</h1>
+    <input type="text" v-model="name" placeholder="Todo name">
+    <input type="text" v-model="description" placeholder="Todo description">
+    <button v-on:click="createTodo">Create Todo</button>
+    <div v-for="item in todos" :key="item.id">
+      <h3>{{ item.name }}</h3>
+      <p>{{ item.description }}</p>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { API } from 'aws-amplify';
+import { createTodo } from './graphql/mutations';
+import { listTodos } from './graphql/queries';
+import { onCreateTodo } from './graphql/subscriptions';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'app',
+  created(){
+    this.getTodos();
+    this.subscribe();
+  },
+  data() {
+    return {
+      name: '',
+      description: '',
+      todos: []
+    }
+  },
+  methods: {
+    subscribe() {
+      API.graphql({ query: onCreateTodo })
+        .subscribe({
+          next: (eventData) => {
+            let todo = eventData.value.data.onCreateTodo;
+            if (this.todos.some(item => item.name === todo.name)) return; // remove duplications
+            this.todos = [...this.todos, todo];
+          }
+        });
+    },
+    async createTodo() {
+      const { name, description } = this;
+      if (!name || !description) return;
+      const todo = { name, description };
+      await API.graphql({
+        query: createTodo,
+        variables: {input: todo},
+      });
+      this.name = '';
+      this.description = '';
+    },
+    async getTodos() {
+      const todos = await API.graphql({
+        query: listTodos
+      });
+      this.todos = todos.data.listTodos.items;
+    }
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
