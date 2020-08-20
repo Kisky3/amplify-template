f641504c41a9a8a7411394124808b21108522779
Kisky3 <kisky3@126.com>
2020-08-20 15:37:40 +0900

    能取到一部分数据

--- todotest/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import Amplify from 'aws-amplify';
-import aws_exports from './aws-exports';
-
-Amplify.configure(aws_exports);
-createApp(App).use(router).mount('#app')
